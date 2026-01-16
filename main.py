21c61d153d97449a3b00aaad41cabbfedda68d80
Artem <69763892+artem9677@users.noreply.github.com>
2026-01-16 20:35:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from telebot import types
 from datetime import datetime
 
 # Токен support-бота (от @BotFather)
-SUPPORT_BOT = telebot.TeleBot("7845313092:AAGDut2c3HQWsagjwfLYk4-aHlGCR332FVQ")  # Замени!
+SUPPORT_BOT = telebot.TeleBot("8013974209:AAGRtOINlIUWAfkqhTw7Qhyt-8HzjVFhR5U")  # Замени!
 
 # Админы (те же, что в основном боте)
-ADMIN_IDS = [6840720060]
+ADMIN_IDS = [8424721481]
 
 # База тикетов: user_id -> list of {'type': 'message/call', 'content': str, 'date': str, 'ticket_id': str}
 support_tickets = {}
